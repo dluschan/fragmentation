4b4a0dd1f911561fdf2dc98f5f5d2bf1d5b96815
Dmitry Luschan <dluschan@gmail.com>
2015-11-22 14:33:23 +0300

    implementation fradmentation count with error

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+template<uint N, uint Base>
+struct FragImpl;
+
 template<uint N>
 struct Capacity
 {
@@ -20,6 +23,87 @@ struct Capacity<0>
 	enum
 	{
 		value = 0
+	};
+};
+
+template<uint N, uint Step>
+struct Loop
+{
+	enum
+	{
+		value = FragImpl<N-Step, Step>::value + Loop<N, Step-1>::value
+	};
+};
+
+template<uint N>
+struct Loop<N, 0>
+{
+	enum
+	{
+		value = FragImpl<N, 0>::value
+	};
+};
+
+template<uint A, uint B>
+struct MIN
+{
+	enum
+	{
+		value = (A < B) ? A : B
+	};
+};
+
+template<uint N, uint Base, bool Condition>
+struct Check
+{
+	enum
+	{
+		value = 0
+	};
+};
+
+template<uint N, uint Base>
+struct Check<N, Base, true>
+{
+	enum
+	{
+		value = Loop<N, MIN<N, Base-1>::value>::value
+	};
+};
+
+template<uint N, uint Base>
+struct FragImpl
+{
+	enum
+	{
+		value = Check<N, Base, N < Capacity<Base>::value>::value
+	};
+};
+
+template<>
+struct FragImpl<0, 0>
+{
+	enum
+	{
+		value = 1
+	};
+};
+
+template<>
+struct FragImpl<0, 1>
+{
+	enum
+	{
+		value = 1
+	};
+};
+
+template<uint N>
+struct Frag
+{
+	enum
+	{
+		value = FragImpl<N, N+1>::value
 	};
 };
 
@@ -46,6 +130,6 @@ uint f(uint n, uint base)
 
 int main()
 {
-	cout << "Емкость блока " << Capacity<11>::value << endl;
+	cout << "Количество вариантов: " << Frag<1>::value << endl;
 	return 0;
 }
